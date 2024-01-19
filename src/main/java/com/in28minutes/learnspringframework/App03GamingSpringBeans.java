1180d0dc1ea19d763c1c374e91ff6153e44d7273
rmsgkr <rmsgkr2@gmail.com>
2024-01-19 14:48:01 +0900

    Beans

---
+++ src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -0,0 +1,33 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+
+
+public class App03GamingSpringBeans {
+
+	public static void main(String[] args) {
+		
+		try(var context = 
+					new AnnotationConfigApplicationContext
+						(GamingConfiguration.class)){
+			context.getBean(GamingConsole.class).up();
+		};
+		
+		
+//		var game = new MarioGame();
+//		var game = new SuperContraGame();
+
+//		var game = new PacmanGame();
+//
+//		var gameRunner = new GameRunner(game);
+//		gameRunner.run();
+
+	}
+
+}
